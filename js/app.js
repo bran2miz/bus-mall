c20b68eb7412568351db30c0925cbf3ebd602ed8
brandonmizutani <bran2miz@gmail.com>
2021-08-17 06:32:28 -0700

    add

--- js/app.js
+++ js/app.js
@@ -1,131 +1,3 @@
-
-// 'use strict';
-
-// Product.allProducts = [];
-// let mainElem = document.getElementById('main')
-// let h1LeftElem = document.getElementById('left-h1');
-// let imgLeftElem = document.getElementById('left-img')
-// let h1MiddleElem = document.getElementById('middle-h1');
-// let imgMiddleElem = document.getElementById('middle-img')
-// let h1RightElem = document.getElementById('right-h1');
-// let imgRightElem = document.getElementById('right-img')
-// let resultsUlElem = document.getElementById('results');
-// let resultsButtonElem = document.getElementById('results-button')
-// let voteCounter = 0;
-// let leftProduct = null;
-// let middleProduct = null;
-// let rightProduct = null;
-
-// function Product(name, identifier, image) {
-// this.name = name;
-// this.productIdentifier = identifier;
-// this.image = image;
-// this.viewedCounter = 0;
-// this.votes = 0;
-
-// Product.allProducts.push(this);
-// }
-
-// function pickThreeProducts() {
-//   let leftProductIndex = Math.floor((Math.random() * Product.allProducts.length));
-//   leftProduct = Product.allProducts[leftProductIndex];
-
-//   let middleProductIndex = Math.floor((Math.random() * Product.allProducts.length));
-//   middleProduct = Product.allProducts[middleProductIndex];
-
-//   while (middleProduct === null || middleProduct === leftProduct) {
-//     let middleProductIndex = Math.floor((Math.random() * Product.allProducts.length));
-//     middleProduct = Product.allProducts[middleProductIndex];
-//   }
-
-//   let rightProductIndex = Math.floor((Math.random() * Product.allProducts.length));
-//   rightProduct = Product.allProducts[rightProductIndex];
-
-//   while (rightProduct === null || rightProduct === middleProduct || rightProduct === leftProduct) {
-//     let rightProductIndex = Math.floor((Math.random() * Product.allProducts.length));
-//     rightProduct = Product.allProducts[rightProductIndex];
-//   }
-
-//   leftProduct.renderProduct(h1LeftElem, imgLeftElem);
-//   middleProduct.renderProduct(h1MiddleElem, imgMiddleElem);
-//   rightProduct.renderProduct(h1RightElem, imgRightElem);
-// }
-
-// Product.prototype.renderProduct = function(h1, img) {
-// h1.textContent = this.name;
-// img.src = this.image;
-// this.viewedCounter++;
-// }
-
-// function handleClick(event) {
-//   let id = event.target.id
-//   if (id === 'left-img' || id === 'middle-img' || id === 'right-img') {
-//     voteCounter++;
-//     console.log(voteCounter);
-//     if (id === 'left-img') {
-//       leftProduct.votes++;
-//     } else if (id === 'middle-img'){
-//       middleProduct.votes++;
-//     } else {
-//       rightProduct.votes++;
-//     }
-//     pickThreeProducts();
-//   } else {
-//     alert('try again');
-//   }
-//   if (voteCounter === 25) {
-//     // renderResults();
-//     renderButton();
-//     // turn off the listener
-//     mainElem.removeEventListener('click', handleClick);
-//   }
-// }
-
-// function handleButtonClick() {
-//   renderResults();
-// }
-
-// function renderButton() {
-//   let buttonElem = document.createElement('button');
-//   buttonElem.textContent = 'View Results';
-//   buttonElem.addEventListener('click', handleButtonClick);
-//   resultsButtonElem.appendChild(buttonElem);
-// }
-
-// function renderResults() {
-//   resultsUlElem.innerHTML = "";
-
-//   for (let product of Product.allProducts) {
-//     let liElem = document.createElement('li');
-//     liElem.textContent = `${product.productIdentifier} had ${product.votes} votes, and was seen ${product.viewedCounter} times.`;
-//     resultsUlElem.appendChild(liElem);
-//   }
-// }
-
-// mainElem.addEventListener('click', handleClick);
-
-// new Products('Banana Slicer', 'banana', 'img/assets/banana.jpg');
-// new Products('Robot Bag', 'bag', 'img/assets/bag.jpg');
-// new Products('Bathroom Ipad Stand','bathroom', 'img/assets/bathroom.jpg');
-// new Products('Open Toe Rain Boots', 'boots' ,'img/assets/boots.jpg');
-// new Products('All-in-one Breakfast Maker', 'breakfast' ,'img/assets/breakfast.jpg');
-// new Products('Meatball Bubblegum', 'bubblegum' ,'img/assets/bubblegum.jpg');
-// new Products('Chair', 'chair' ,'img/assets/chair.jpg');
-// new Products('Cthulu Action Figure', 'cthulhu', 'img/assets/cthulhu.jpg');
-// new Products('Duck Bill Dog Muzzle', 'dog-duck' ,'img/assets/dog-duck.jpg');
-// new Products('Dragon Meat', 'dragon' ,'img/assets/dragon.jpg');
-// new Products('Utensil Pens', 'pen' ,'img/assets/pen.jpg');
-// new Products('Pet Sweeper', 'pet-sweep' ,'img/assets/pet-sweep.jpg');
-// new Products('Pizza Scissors', 'scissors' ,'img/assets/scissors.jpg');
-// new Products('Shark Sleeping Bag', 'shark' ,'img/assets/shark.jpg');
-// new Products('Baby Onesie Sweeper', 'sweep' ,'../img/assets/sweep.png');
-// new Products('Tauntaun Sleeping Bag', 'tauntaun' ,'../img/assets/tauntaun.jpg');
-// new Products('Unicorn Meat', 'unicorn','../img/assets/unicorn.jpg');
-// new Products('Perpetual Watering Can', 'water-can','../img/assets/water-can.jpg');
-// new Products('Wine Glass With Hole', 'wine-glass','../img/assets/wine-glass.jpg');
-
-// pickThreeProducts();
-
 'use strict';
 
 // global variables -----------------------------------------------------------
@@ -142,7 +14,7 @@ let clicksAllowed = 25;
 // constructor
 function Products(name, fileExtension = 'jpg') {
   this.name = name;
-  this.src = `../img/assets/${name}.${fileExtension}`;
+  this.src = `img/assets/${name}.${fileExtension}`;
   this.views = 0;
   this.clicks = 0;
   allProducts.push(this);
@@ -217,7 +89,6 @@ new Products ('tauntaun');
 new Products ('unicorn');
 new Products ('water-can');
 new Products ('wine-glass');
-
 
 console.log(allProducts);
 renderProducts();
